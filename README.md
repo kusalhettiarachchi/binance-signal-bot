9e8560b45de08c86e9d129f333220609ee52c077
Kusal Hettiarachchi <kusal.14@cse.mrt.ac.lk>
2022-10-04 23:13:54 +1100

    bashaway changes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # binance-signal-bot
 A telegram bot for providing price signals for crypto markets listed in  `Binance` exchange
+
+## how to run
+
+1. create a virtual environment, activate it. (check your operating system specific instructions. the following is good if you're running OSX and *nix)
+
+    `$ virtualenv .venv`
+    `$ . .venv/bin/activate`
+
+2. install the packages
+
+    `$ pip install -r requirements.txt`
+
+3. run the `main.py`
+
+    `$ python main.py`
+
+4. check the `bot.log` for logs
+
+    `$ tail -f bot.log`
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-import os, sys, threading, logging
+import os, sys, logging
 from datetime import datetime as dt
 
 from dotenv import load_dotenv
@@ -12,6 +12,7 @@ logging.basicConfig(filename='bot.log', level=logging.DEBUG,
                     datefmt='%m/%d/%Y %I:%M:%S %p',
                     filemode='w')
 
+
 ######################################
 # data structures for containing data 
 ######################################
@@ -19,59 +20,69 @@ klines      = dict()
 processors  = dict()
 listeners   = dict()
 
+
 ###################
 # helper functions
 ###################
-def process_klines(s):
-    # todo: build the data structure here
-    klines[s] = client.get_historical_klines(s, Client.KLINE_INTERVAL_1MINUTE, "99 minutes ago UTC")
-    logging.info('processing %d candles', len(klines[s]))
-
-
 def process_ws_message(msg):
+    """place holder method for processing the event sent by the exchange. 
+    you can use this function as a starting point to implement your trading strategy
+
+    Args:
+        msg (dict): event sent by the exchange
+    """
     logging.info("stream: {}-{} data: {}".format(msg['s'], msg['e'], str(msg)))
 
-# read .env properties
-load_dotenv()
 
-# read secrets from the environment
-api_key     = os.getenv('APIKEY')
-api_secret  = os.getenv('APISECRET')
-symbols     = os.getenv('SUBSCRIPTIONS').split(',')
-timeout     = int(os.getenv('WSTIMEOUT'))
-logging.info("auth config successfully read")
+def load_config():
+    """loads the configuration from the .env file
 
-# greet
-logging.info("welcome! started bot ...........")
-
-# create client, test REST API, and exit if a connection cannot be established
-client = Client(api_key, api_secret)
-if not client.ping() == {}:
-    logging.error("failed to ping the server. check network!")
-    sys.exit(-1)
-logging.debug("server ping successful")
-
-# check server health
-time_res = client.get_server_time()
-logging.debug("server time %s", str(dt.fromtimestamp(int(time_res['serverTime'])/1000)))
-
-status = client.get_system_status()
-if not status['status'] == 0:
-    logging.error("server not in operational level. received status %s", status['msg'])
-    sys.exit(-1)
-
-logging.debug("server operating normally")
+    Returns:
+        api_key (str): KEY of the binance API secret
+        api_secret (str): SECRET of the binance API secret
+        subscriptions (list[str]): list of trading pairs to subscribe to 
+        timeout (int): socket timeout
+    """
+    # read .env properties
+    load_dotenv()
+    # read secrets from the environment
+    return os.getenv('APIKEY'), os.getenv('APISECRET'), os.getenv('SUBSCRIPTIONS').split(','), int(os.getenv('WSTIMEOUT'))
 
 
 async def main():
+    # greet
+    logging.info("welcome! started bot. reading config ...........")
+
+    # load config
+    key, secret, symbols, timeout = load_config()
+
+    if not (key and secret and symbols and timeout):
+        logging.error("failed to read auth config")
+        sys.exit(1)
+    logging.info("auth config successfully read")
+
+    # create client and exit if a connection cannot be established
+    client = Client(key, secret)
+    if not client.ping() == {}:
+        logging.error("failed to ping the server. check network!")
+        sys.exit(1)
+    logging.debug("server ping successful")
+
+    # check server health
+    time_res = client.get_server_time()
+    logging.debug("server time %s", str(dt.fromtimestamp(int(time_res['serverTime'])/1000)))
+
+    status = client.get_system_status()
+    if not status['status'] == 0:
+        logging.error("server not in operational level. received status %s", status['msg'])
+        sys.exit(1)
+    logging.debug("server operating normally")
+
     # create the websocket client
     bm = BinanceSocketManager(client, user_timeout=timeout)
 
-    # fetch 1 minute klines for the last day up until now for each symbol
+    # for each trading pair configured, open a connection and process using the defined function
     for s in symbols:
-        processors[s+'_klines'] = threading.Thread(target=process_klines, args=(s,))
-        processors[s+'_klines'].start()
-        processors[s+'_klines'].join()
         # start any sockets here, i.e a trade socket
         ts = bm.trade_socket(s)
         # then start receiving messages
@@ -79,12 +90,9 @@ async def main():
             while True:
                 res = await tscm.recv()
                 process_ws_message(res)
-
-        # listeners[s] = bm.symbol_ticker_socket(s, process_ws_message)
-
-    logging.debug('processed klines')
-    # then start the socket manager
+    # finally start the socket manager
     bm.start()
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
